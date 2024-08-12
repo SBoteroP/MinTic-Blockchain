910e1e61d6cc824b8123f61e4ad31bd4db7c51f2
SBoteroP <santibote0987@gmail.com>
2024-08-11 20:31:41 -0500

    Actualización de bloqueo de retiros.

--- contracts/Depositos/CryptoDeposit.sol
+++ contracts/Depositos/CryptoDeposit.sol
@@ -14,6 +14,45 @@ contract CryptoDeposit {
     // se utiliza un mapeo bidimensional para almacenar las cuentas de los usuarios
     mapping(address => mapping(bytes10 => Cuenta)) cuentas;
 
+    uint256 public tiempoFinBloqueo;
+    bool public bloqueoPermanente;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // funcion que se encarga de revisar el dueño actual del contrato
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Solo el propietario puede ejecutar esta funcion"
+        );
+        _;
+    }
+
+    modifier retirosDeshabilitados() {
+        require(
+            bloqueoPermanente,
+            "Los retiros estan bloqueados permanentemente"
+        );
+        require(
+            block.timestamp <= tiempoFinBloqueo,
+            "Los retiros estan temporalmente bloqueados"
+        );
+        _;
+    }
+
+    // función para transferir la propiedad
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(
+            newOwner != address(0),
+            "Nueva direccion de propietario invalida"
+        );
+        owner = newOwner;
+    }
+
     // permite a los usuarios depositar una cantidad específica de alguna criptomoneda
     function Depositar(uint256 cantidad, bytes10 moneda) external payable {
         require(msg.value == cantidad, "Cantidad no coincidente..."); // si la variable local 'value' es igual a la cantidad
@@ -33,7 +72,10 @@ contract CryptoDeposit {
     }
 
     // permite a los usuarios retirar una cantidad específica de alguna criptomoneda
-    function retirar(uint256 cantidad, bytes10 moneda) external {
+    function retirar(uint256 cantidad, bytes10 moneda)
+        external
+        retirosDeshabilitados
+    {
         Cuenta storage cuenta = cuentas[msg.sender][moneda];
         require(cuenta.saldo >= cantidad, "Saldo insuficiente");
 
@@ -45,5 +87,22 @@ contract CryptoDeposit {
         if (cuenta.saldo == 0) {
             delete cuentas[msg.sender][moneda];
         }
+    }
+
+    // permite al dueño del contrato bloquear los retiros temporalmente.
+    function bloquearRetirosTemp(uint256 tiempoBloqueo) public onlyOwner {
+        require(!bloqueoPermanente, "Los retiros ya se encuentran bloqueados.");
+        tiempoFinBloqueo = block.timestamp + tiempoBloqueo;
+    }
+
+    // permite al dueño del contrato bloquear los retiros permanentemente.
+    function bloquearRetirosPerma() public view onlyOwner {
+        bloqueoPermanente == true;
+    }
+
+    // permite al dueño del contrato retaurar el retiro de activos del contrato.
+    function restaurarRetiros() public onlyOwner {
+        bloqueoPermanente = false;
+        tiempoFinBloqueo = block.timestamp;
     }
 }
